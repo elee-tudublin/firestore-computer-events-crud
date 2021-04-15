e4f4fd0cc05b613dca051dcb7c5700799facd123
Enda Lee <enda.lee@tudublin.ie>
2021-04-15 14:35:22 +0100

    move to public folder

---
+++ public/src/database/config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDWl7PtOKw5bLgeh-3CHVHQXwyFOyxG8FI",
+    authDomain: "elee-event-log.firebaseapp.com",
+    projectId: "elee-event-log",
+    storageBucket: "elee-event-log.appspot.com",
+    messagingSenderId: "716677012911",
+    appId: "1:716677012911:web:fb542249b93b99992b635a",
+    measurementId: "G-JZQ2V7NRQD"
+  };
